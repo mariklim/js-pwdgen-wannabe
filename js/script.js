78f65d3ba1dfc1ddd568b2d4df394d147e62c1a1
mariklim <maria.klimenko889@gmail.com>
2021-07-28 16:00:45 +0200

    add js rules

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var firstnName = prompt("Inserisci il tuo nome");
+var secondName = prompt("Inserisci il tuo cognome");
+var color = prompt("Inserisci il tuo colore preferito");
+document.getElementById("password").innerHTML += (firstnName + secondName + color) + 40;
